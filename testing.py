corrected jacobian in derivative evaluation

testing.py
@@ -1,9 +1,11 @@
 import tools as tls
 import numpy as np
 
+from cycling import Cycling
+
 import matplotlib.pyplot as plt
 
-def test_interpolation():
+def test_interpolation_00():
     x = np.arange(3.,7.)
     y = np.array([2.,1.,1.,2.])
 
@@ -21,11 +23,59 @@ def test_interpolation():
     taylor = f(x0) + df(x0) * (sample-x0)
 
 
-    plt.plot(np.linspace(np.amin(x),np.amax(x)),f(sample))
-    plt.plot(np.linspace(np.amin(x),np.amax(x)),taylor,'-g')
+    plt.plot(np.linspace(np.amin(x),
+             np.amax(x)),f(sample))
+    plt.plot(np.linspace(
+             np.amin(x),np.amax(x)),
+             taylor,'-g')
     plt.plot(x,y,'or')
     plt.show()
 
+def test_interpolation_01():
+    x = np.array([ 1., 2., 4., 7., 9., 11.])
+    normalize = tls.normalize_function(x)
+    y = np.array([ 4.38,8.98, 18.46, 33.23, 43.89, 54.7 ])
+    y_prime = np.array([4.255, 4.535, 4.936, 5.076 ,5.095, 5.104])
+
+    space = tls.get_bernsetin_order_two()#[tls.bernstein_polynomial(2,i) for i in range(3)]
+    space_prime = tls.get_bernsetin_derivs_order_two()
+
+    coeffs = tls.least_squares(x,y,space)
+    print(coeffs)
+
+    f = tls.get_function(coeffs,space)
+    df = tls.get_function(coeffs,space_prime)
+
+
+    print("df(.0) = ",df(.5))
+    print(" f(.0) = ", f(.5))
+
+    x0 = normalize(np.amin(x))
+    sample = np.linspace(0,1)
+    taylor = f(x0) + df(x0) * (sample-x0)
+    #print(df(x0))
+
+    x0 = np.linspace(0,1,10)
+
+    w = np.amax(x)-np.amin(x)
+
+    plt.plot(x0 * w + np.amin(x),f(x0),"b")
+    plt.plot(x0 * w + np.amin(x),df(x0)/w,"r")
+    plt.plot(x,y,'ob')
+    plt.plot(x,y_prime,'or')
+    plt.show()
+
+def test_iterator():
+    av = Cycling()
+    av.read_data('7C9A5608.FIT')
+    f = av.interpolate_and_derive_data()
+    print(f[:6])
+    print(av.data["timestamp"][:6])
+    print(av.data["distance"][:6])
+    print(av.data["speed"][:6])
+
+
 
 if __name__ == "__main__":
-    test_interpolation()
+    test_interpolation_01()
+    #test_iterator()

tools.py
@@ -11,6 +11,11 @@ def bernstein_polynomial(n,i):
         return poly
     return lambda t : func(n,i,t)
 
+def normalize_function(v):
+    v_min = np.amin(v)
+    v_max = np.amax(v)
+    return lambda x : (x - v_min)/(v_max-v_min)
+
 def normalize(v):
     v = v - np.amin(v)
     v = v/np.amax(v)
@@ -35,12 +40,16 @@ def get_bernsetin_order_one():
     return [lambda x : 1-x,
             lambda x : x]
 
+def get_bernsetin_derivs_order_one():
+    return [lambda x : -1.,
+            lambda x : 1.]
+
 def get_bernsetin_order_two():
     return [lambda x : (1-x)*(1-x),
             lambda x : 2*x*(1-x),
             lambda x : x*x]
 
-def get_bernsetin_derivs_orde_two():
+def get_bernsetin_derivs_order_two():
     return [lambda x : -2*(1-x),
             lambda x : 2*(1-2*x),
             lambda x : 2*x]
@@ -58,9 +67,6 @@ def get_function(coeffs,space):
 
 def interpolate_and_get_derivative(x,y):
     space = get_bernsetin_order_one()
-
-
+    space_prime = get_bernsetin_derivs_order_one()
     coeffs = least_squares(x,y,space)
-
-    print coeffs
-    return df(sample)
+    return get_function(coeffs,space_prime)

training.py
@@ -8,6 +8,7 @@ class Training(object):
         self.data = {}
         self.data_length = 0
         self.i = 0
+        self.stencil_size = 5
 
     def read_data(self, filename):
         fitfile_path = os.path.join('data', filename)
@@ -59,12 +60,15 @@ class Training(object):
 
         if self.i < self.data_length:
             i = self.i
-            if i < 2:
-                self.stencil_ids = np.arange(i,3+i)
-            elif i > len(self.data["timestamp"])-3:
-                self.stencil_ids = np.arange(i-2,i+1)
+            s = self.stencil_size
+            s_fw = np.int32(np.floor(s/2))
+            s_bk = np.int32(np.ceil(s/2))
+            if i < s:
+                self.stencil_ids = np.arange(i,s+i)
+            elif i > len(self.data["timestamp"])-s:
+                self.stencil_ids = np.arange(i-s_bk-1,i+s_fw-1)
             else:
-                self.stencil_ids  = np.arange(i-3,i+2)
+                self.stencil_ids  = np.arange(i-s,i)
             self.i += 1
             return self
         else:
@@ -86,5 +90,8 @@ class Training(object):
         for s in self:
             x = self.data["timestamp"][self.stencil_ids]
             y = self.data["distance"][self.stencil_ids]
-            f.append(tls.interpolate_and_get_derivative(x,y,.5))
+            sample = .5
+            w = np.amax(x)-np.amin(x)
+            der = tls.interpolate_and_get_derivative(x,y)(sample)/w
+            f.append(der)
         return np.array(f)